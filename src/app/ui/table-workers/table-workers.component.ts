9c011e057e971c2722204201e834515492d945c8
Артём Усвяцов <artoumy@yandex.ru>
2020-06-10 06:50:47 +0300

    first commit

---
+++ src/app/ui/table-workers/table-workers.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { MyWorker, MyWorkerType } from 'src/app/shared/worker.model';
+
+@Component({
+  selector: 'app-table-workers',
+  templateUrl: './table-workers.component.html',
+  styleUrls: ['./table-workers.component.css'],
+})
+export class TableWorkersComponent implements OnInit {
+  @Input() title: string;
+  @Input() workers: MyWorker[] = [];
+
+  @Output() deleteWorker = new EventEmitter<number>();
+  @Output() editWorker = new EventEmitter<MyWorker>();
+  
+  workerType = MyWorkerType;
+  name: string;
+  surname: string;
+  type = this.title;
+  
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  onDeleteWorker(id: number) {
+    this.deleteWorker.emit(id);
+  }
+
+  onEditWorker(worker : MyWorker)
+  {
+    this.editWorker.emit(worker);
+  }
+}
